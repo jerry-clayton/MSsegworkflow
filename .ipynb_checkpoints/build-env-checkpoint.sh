6eb9eff0f40a524b8a907a0f2f2a4c48e6660124
jclayton0 <jeremiah.clayton36@myhunter.cuny.edu>
2024-10-17 17:50:08 -0700

    add build scripts

---
+++ .ipynb_checkpoints/build-env-checkpoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S bash --login
+##!/bin/bash
+
+set -euo pipefail
+
+basedir=$( cd "$(dirname "$0")" ; pwd -P)
+echo "Creating conda environment from ${basedir}/ms-env.yml"
+conda env create --name "ms-env" -f ${basedir}/ms-env.yml
+#conda env create -f ms-env.yml
+Rscript --verbose create-ms-env.R
