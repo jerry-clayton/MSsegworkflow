fc140e760903425fb575086f9e53dee09e61396d
jclayton0 <jeremiah.clayton36@myhunter.cuny.edu>
2024-10-20 14:10:33 -0700

    remove debug code

--- .ipynb_checkpoints/sm-tile-checkpoint.sh
+++ .ipynb_checkpoints/sm-tile-checkpoint.sh
@@ -33,12 +33,12 @@ testout="test_output_${new_infile}"
 output_path="$parent_path/output/$outfile"
 test_output_path="$parent_path/output/$testout"
 
-cp "$input_path" "$test_output_path"
+#cp "$input_path" "$test_output_path"
 echo "input path: $input_path"
 echo "output path: $output_path"
 # echo "Running r package install script" 
 # # Run script to install r-only packages
-# Rscript --verbose ${basedir}/run/create-ms-env.R
+#Rscript --verbose ${basedir}/run/create-ms-env.R
 
 # # Check if the R script ran successfully
 # if [[ $? -ne 0 ]]; then
@@ -49,7 +49,7 @@ echo "output path: $output_path"
 # echo "R package install script ran succedssfully."
 echo "Running  MeanShift segmentation"
 # Run the R script
-# Rscript --verbose ${basedir}/run/automate-teak-sm-tiles-maap.R "$input_path" "$output_path"
+Rscript --verbose ${basedir}/run/automate-teak-sm-tiles-maap.R "$input_path" "$output_path"
 
 # Check if the R script ran successfully
 if [[ $? -ne 0 ]]; then
