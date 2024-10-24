7d8f9f3af23c681016596cf27275b0b5e2df4ec6
jclayton0 <jeremiah.clayton36@myhunter.cuny.edu>
2024-10-24 10:30:47 -0700

    fix conda activate error

--- .ipynb_checkpoints/build-env-checkpoint.sh
+++ .ipynb_checkpoints/build-env-checkpoint.sh
@@ -8,6 +8,8 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P)
 echo "Creating conda environment from ${basedir}/ms-env.yml"
 conda env create --name "ms-env" -f ${basedir}/ms-env.yml
 
+source $(conda info --base)/etc/profile.d/conda.sh
+eval "$(conda shell.bash hook)"
 conda activate ms-env
 
 echo "Current Conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
